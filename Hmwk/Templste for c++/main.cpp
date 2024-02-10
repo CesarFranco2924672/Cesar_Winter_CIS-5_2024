72dc09f1ddd63f8232556708c6319924b131306c
cesar167088 <cfranco40@student.rccd.edu>
2024-02-10 00:48:19 -0800

    Signed-off-by: cesar167088 <cfranco40@student.rccd.edu>

---
+++ Hmwk/Templste for c++/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Cesar Franco
+ * Created on January 4, 2024, 1:53 PM
+ * Purpose: Template for c++
+ */
+
+//System Libraries
+#include <iostream> //Input-Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math, Physics, Chemistry
+
+//Function Prototypes
+
+//Program Execution Begins Here
+int main(int argc, char** argv)
+    {
+    //Set a random seed
+    
+    //Declare all Variables
+    
+    //Initialize all Variables
+            
+    //Process or Map Solutions
+    
+    //Display the Output
+    
+    //Exit the Program
+    return 0;
+    }
+
